bd5ffd15f47ab88e5c815be0d0b494b7b3ae19f2
shaanvokounucsb <svokoun@ucsb.edu>
2026-05-24 15:49:12 -0700

    Create utilities.h

---
+++ utilities.h
@@ -0,0 +1,11 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <string>
+
+using namespace std;
+
+string trimAndLowercase(const string& str);
+string cleanPrefixOnly(const string& str);
+
+#endif
