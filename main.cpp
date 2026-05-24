2dea20bd38ab0cd75b1112a135a89a3ed1969a47
shaanvokounucsb <svokoun@ucsb.edu>
2026-05-24 16:16:42 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -95,7 +95,7 @@ bool parseLine(string &line, string &movieName, double &movieRating) {
     }
     string cleanName = "";
     for (size_t i = 0; i < movieName.length(); ++i) {
-        if (movieName[i] != ":"){
+        if (movieName[i] != ':'){
             cleanName += movieName[i];
         }
     }
